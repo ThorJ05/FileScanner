dd1846b0f442c7a5bb779542de6b28e9288413ed
thor <thor.mathiaskn@gmail.com>
2026-04-21 10:34:55 +0200

    3 lags arkitektur

--- src/main/java/com/example/filescanner/GUI/HelloController.java
+++ src/main/java/com/example/filescanner/GUI/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.filescanner;
+package com.example.filescanner.GUI;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.filescanner {
 
     opens com.example.filescanner to javafx.fxml;
     exports com.example.filescanner;
+    exports com.example.filescanner.GUI;
+    opens com.example.filescanner.GUI to javafx.fxml;
 }
