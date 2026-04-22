f7a17d76c631584e394728956c1c53096db36937
budabob1901 <kjemsfrederik@gmail.com>
2026-04-22 19:25:34 +0200

    Have made BEE for Abstract user, userrole. so we can makw the admin and user classes

---
+++ src/main/java/com/example/filescanner/BEE/User.java
@@ -0,0 +1,34 @@
+package com.example.filescanner.BEE;
+
+public abstract class User {
+
+    private final String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private final UserRole role;
+
+   //constructor
+    protected User(String id, String firstName, String lastName, String email, String password, UserRole role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters
+    public String getId()                       {return id;}
+    public String getFirstName()                {return firstName;}
+    public String getLastName()                 {return lastName;}
+
+    // Setters
+    public void setFirstName(String fn)         {this.firstName = fn;}
+    public void setLastName(String ln)          {this.lastName = ln;}
+    public void setEmail(String em)             {this.email = em;}
+    public void setPassword(String pw)          {this.password = pw;}
+
+    public boolean checkPassword(String pw)     {return this.password != null && this.password.equals(pw);}
+}
---
+++ src/main/java/com/example/filescanner/BEE/UserRole.java
@@ -0,0 +1,15 @@
+package com.example.filescanner.BEE;
+
+public enum UserRole
+{
+    USER( "Bruger"),
+    ADMIN("Admin");
+
+
+    private final String displayName;
+
+    UserRole(String displayName) {this.displayName = displayName;}
+
+    public String getDisplayName() {return displayName;}
+}
+
